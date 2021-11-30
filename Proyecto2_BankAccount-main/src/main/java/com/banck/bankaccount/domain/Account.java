dd8f9290ccf6ec0334b0bc091d76999670691c83
joseniquen <62155219+joseniquen@users.noreply.github.com>
2021-11-30 11:35:05 -0500

    g
    g

---
+++ Proyecto2_BankAccount-main/src/main/java/com/banck/bankaccount/domain/Account.java
@@ -0,0 +1,20 @@
+package com.banck.bankaccount.domain;
+
+import lombok.Data;
+
+/**
+ *
+ * @author jonavcar
+ */
+@Data
+public class Account {
+
+    public String account;
+    public String customer;
+    public String customerType;
+    public String accountType;
+    public String dateCreated;
+    public double commission;
+    public int movementLimit;
+    public String dateMovement;
+}
---
+++ Proyecto2_BankAccount-main/src/main/java/com/banck/bankaccount/infraestructure/model/dao/AccountDao.java
@@ -0,0 +1,24 @@
+package com.banck.bankaccount.infraestructure.model.dao;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+/**
+ *
+ * @author jonavcar
+ */
+@Data
+@Document("account")
+public class AccountDao {
+
+    @Id
+    public String account;
+    public String customer;
+    public String customerType;
+    public String accountType;
+    public String dateCreated;
+    public double commission;
+    public int movementLimit;
+    public String dateMovement;
+}
